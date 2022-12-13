e1184e23735ec66cdb79c43007fa1751482b8d8a
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-13 19:26:40 +0700

    creae feed post component

--- components/feed/Feed.tsx
+++ components/feed/Feed.tsx
@@ -1,7 +1,32 @@
 import { SparklesIcon } from "@heroicons/react/24/outline"
 import FeedInput from "./FeedInput"
+import FeedPost from "./FeedPost"
 
 export default function Feed() {
+  const posts = [
+    {
+      id: 1,
+      userProfileImg:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png",
+      displayName: "John Doe",
+      username: "johndoe",
+      image:
+        "https://images.unsplash.com/photo-1670881528060-17142d874661?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      text: "Museum",
+      timestamp: "1h"
+    },
+    {
+      id: 2,
+      userProfileImg:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png",
+      displayName: "Imanuel",
+      username: "Imanul",
+      image:
+        "https://images.unsplash.com/photo-1670443373385-490a6fb4d73b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      text: "Bandara Keren",
+      timestamp: "1h"
+    }
+  ]
   return (
     <div className="sm:ml-20 xl:ml-72 border-l border-r border-gray-200 xl:min-w-[576px] flex-grow max-w-2xl">
       <div className="flex items-center py-2 px-3 top-0 sticky z-10 bg-white border-b border-gray-200">
@@ -11,6 +36,9 @@ export default function Feed() {
         </div>
       </div>
       <FeedInput />
+      {posts.map((post) => (
+        <FeedPost key={post.id} post={post} />
+      ))}
     </div>
   )
 }
---
+++ components/feed/FeedPost.tsx
@@ -0,0 +1,45 @@
+import {
+  ChartBarIcon,
+  ChatBubbleLeftIcon,
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon
+} from "@heroicons/react/24/outline"
+
+export default function FeedPost({ post }: any) {
+  return (
+    <div className="flex w-full px-10 py-3 cursor-pointer border-b border-gray-200">
+      {/* userProfileImg */}
+      <img src={post.userProfileImg} alt="userProfileImg" className="w-10 h-10 rounded-full mt-2 mr-4" />
+      {/* right side */}
+      <div className="w-full">
+        {/* header */}
+        <div className="flex items-center justify-between">
+          {/* userInfo */}
+          <div className="flex space-x-1 items-center whitespace-nowrap">
+            <h4 className="font-bold text-base hover:underline">{post.displayName}</h4>
+            <p className="text-sm sm:text-base">@{post.username} - </p>
+            <span className="text-sm sm:text-base hover:underline">{post.timestamp}</span>
+          </div>
+          {/* dot icon */}
+          <EllipsisHorizontalIcon className="h-14 w-14 p-2 hoverEffect hover:bg-sky-100 hover:text-sky-500" />
+        </div>
+        {/* post text */}
+        <p className="text-gray-800 text-base mb-2">{post.text}</p>
+
+        {/* post img */}
+        <img className="rounded-2xl mr-2 w-full" src={post.image} alt="postImg" />
+
+        {/* icons */}
+        <div className="flex justify-between text-gray-500 p-2">
+          <ChatBubbleLeftIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="w-9 h-9 hoverEffect p-1 hover:text-red-500 hover:bg-red-100" />
+          <HeartIcon className="w-9 h-9 hoverEffect p-1 hover:text-red-500 hover:bg-red-100" />
+          <ShareIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  )
+}
