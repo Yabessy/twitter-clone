b2c09f35e1b5179fa0f9b1da9a22df49c8e0c2da
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-15 08:52:07 +0700

    create complete like functionality

--- components/feed/FeedPost.tsx
+++ components/feed/FeedPost.tsx
@@ -6,9 +6,55 @@ import {
   ShareIcon,
   TrashIcon
 } from "@heroicons/react/24/outline"
+import { HeartIcon as FilledHeartIcon } from "@heroicons/react/24/solid"
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc
+} from "firebase/firestore"
+import { useEffect, useState } from "react"
 import Moment from "react-moment"
+import { useRecoilState } from "recoil"
+import { db } from "../../firebase"
+import { userState } from "../atom/userAtom"
+import { useRouter } from "next/router"
 
 export default function FeedPost({ post }: any) {
+  const router = useRouter()
+  const [currentUser] = useRecoilState(userState)
+  const [likes, setLikes] = useState([])
+  const [liked, setLiked] = useState(false)
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "tweets", post.id, "likes"),
+      (snapshot: any) => {
+        setLikes(snapshot.docs)
+      }
+    )
+  }, [db])
+  useEffect(() => {
+    // @ts-ignore
+    setLiked(likes.findIndex((like) => like.id === currentUser?.uid) !== -1)
+  }, [likes])
+  async function likeTweet() {
+    if (!currentUser) {
+      router.push("/signin")
+      return
+    } else {
+      if (liked) {
+        // @ts-ignore
+        await deleteDoc(doc(db, "tweets", post.id, "likes", currentUser?.uid))
+      } else {
+        // @ts-ignore
+        await setDoc(doc(db, "tweets", post.id, "likes", currentUser?.uid), {
+          // @ts-ignore
+          username: currentUser?.username
+        })
+      }
+    }
+  }
   return (
     <div className="flex w-full pl-3 pr-10 py-3 cursor-pointer border-b border-gray-200">
       {/* userProfileImg */}
@@ -48,7 +94,27 @@ export default function FeedPost({ post }: any) {
         <div className="flex justify-between text-gray-500 p-2 mt-1">
           <ChatBubbleLeftIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="w-9 h-9 hoverEffect p-1 hover:text-red-500 hover:bg-red-100" />
-          <HeartIcon className="w-9 h-9 hoverEffect p-1 hover:text-red-500 hover:bg-red-100" />
+          <div className="flex">
+            {liked ? (
+              <FilledHeartIcon
+                onClick={() => likeTweet()}
+                className="w-9 h-9 hoverEffect p-1 text-red-500 hover:text-red-500 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={() => likeTweet()}
+                className="w-9 h-9 hoverEffect p-1 hover:text-red-500 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`text-base ${
+                  liked ? "text-red-500" : "text-gray-500"
+                } font-bold`}>
+                {likes.length}
+              </span>
+            )}
+          </div>
           <ShareIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="w-9 h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
         </div>
